91b81c0694d685fa32653956759380e21f91eb6d
nondefault <spam@nondefault.net>
2017-08-23 03:02:50 -0500

    Working commit (broken)

--- src/server/client/slider-plus.js
+++ src/server/client/slider-plus.js
@@ -3,12 +3,12 @@ Vue.component("slider-plus", {
 	<table style="border-spacing: 0; width: 100%;">
 		<tr>
 			<td>{{label}}</td>
-			<td style="width: 100%;">
-				<input type="range" class="mdl-slider mdl-js-slider" :min="min" :max="max" :step="step" v-model.number="v"/>
+			<td style="width: 100%;" ref="slContainer">
+				<input type="range" class="mdl-slider mdl-js-slider" :min="min" :max="max" :step="step" v-on:input="sliderInput"/>
 			</td>
 			<td>
-				<div class="mdl-textfield mdl-js-textfield" style="width: 80px;">
-					<input class="mdl-textfield__input" type="text" v-model.number="v" pattern="-?[0-9]*(\.[0-9]+)?">
+				<div class="mdl-textfield mdl-js-textfield" ref="tfContainer" style="width: 80px;">
+					<input class="mdl-textfield__input" type="text" v-on:input="textInput" pattern="-?[0-9]*(\.[0-9]+)?">
 				</div>
 			</td>
 		</tr>
@@ -21,10 +21,37 @@ Vue.component("slider-plus", {
 		}
 	},
 	methods: {
+		sliderInput: function(val) {
+			let x = this.inputHandler(val);
+			this.setText(x);
+		},
+		textInput: function(val) {
+			let x = this.inputHandler(val);
+			this.setSlider(x);
+		},
+		inputHandler: function(x) {
+			let numeric = Number.parseFloat(x);
+			if (!Number.isNaN(numeric)) {
+				this.v = numeric;
+				this.$emit("input", numeric);
+				return numeric;
+			}
+			return this.v;
+		},
+		setSlider: function(val) {
+			let slider = this.$refs.slContainer.getElementsByTagName("input")[0];
+			slider.MaterialSlider.change(val);
+		},
+		setText: function(val) {
+			let field = this.$refs.tfContainer;
+			field.MaterialTextfield.change(val);
+		}
 	},
 	watch: {
-		v: function(newValue) {
-			this.$emit("input", newValue);
+		value: function(newValue) {
+			this.v = newValue;
+			this.setSlider(newValue);
+			this.setText(newValue);
 		}
 	}
 });
